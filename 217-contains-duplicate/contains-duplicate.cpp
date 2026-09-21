49e7a43f7f358977c50421c784d5801d904c31b5
ayazsk1890-wq <ayazsk1890@gmail.com>
2026-09-21 22:18:04 +0530

    Time: 88 ms (16.58%) | Memory: 82.7 MB (95.84%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,9 +1,44 @@
 class Solution {
+private:
+    void heapify(std::vector<int>& arr, int n, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && arr[left] > arr[largest]) {
+            largest = left;
+        }
+
+        if (right < n && arr[right] > arr[largest]) {
+            largest = right;
+        }
+
+        if (largest != i) {
+            std::swap(arr[i], arr[largest]);
+            heapify(arr, n, largest);
+        }
+    }
+
+    void heapSort(std::vector<int>& arr) {
+        int n = arr.size();
+
+    
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+
+        
+        for (int i = n - 1; i > 0; i--) {
+            std::swap(arr[0], arr[i]);
+            heapify(arr, i, 0);
+        }
+    }
+
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin() , nums.end());
-        for(int i = 1 ; i < nums.size() ;i++ ){
-            if(nums[i] == nums[i -1]){
+        heapSort(nums);
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] == nums[i - 1]) {
                 return 1;
             }
         }
