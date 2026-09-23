9fd05db7bc22237360ac93abf4bc66deafebf67d
ayazsk1890-wq <ayazsk1890@gmail.com>
2026-09-23 22:36:48 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (48.03%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int count[26] = {0};
+
+        if (s.length() != t.length()) {
+            return 0;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            count[s[i] - 'a']++;
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            count[t[i] - 'a']--;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) {
+                return 0;
+            }
+        }
+
+        return 1;
+    }
+};
